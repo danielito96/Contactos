e82ea89c445078c3e935855cdbe4e7c4a9a56692
danielito96 <daniel_m9603@hotmail.com>
2018-07-17 22:41:27 -0500

    Leer base de datos

--- Contactos/MainPage.xaml.cs
+++ Contactos/MainPage.xaml.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contactos.Clases;
 using Xamarin.Forms;
 
 namespace Contactos
 {
     public partial class MainPage : ContentPage
     {
+        List<Contacto> contactos;
+
         public MainPage()
         {
             InitializeComponent();
+
+            contactos = new List<Contacto>();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using(var conn = new SQLite.SQLiteConnection(App.RUTA_BD))
+            {
+                conn.CreateTable<Contacto>();
+                contactos = conn.Table<Contacto>().ToList();
+
+            }
         }
 
         private void ToolbarItem_Clicked(Object sender, EventArgs args)
