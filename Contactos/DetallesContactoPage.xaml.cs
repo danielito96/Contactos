81c79185d067df9a21a353c9d6bb782190d826ea
danielito96 <daniel_m9603@hotmail.com>
2018-07-19 17:13:23 -0500

    Navegar a detalles contacto
    Se navega a detalles contacto desde la página principal seleccionanda para saber la información de lo que el usuario quiere ver

--- Contactos/DetallesContactoPage.xaml.cs
+++ Contactos/DetallesContactoPage.xaml.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
-
+using Contactos.Clases;
 using Xamarin.Forms;
 
 namespace Contactos
 {
     public partial class DetallesContactoPage : ContentPage
     {
-        public DetallesContactoPage()
+        public Contacto Contacto
+        {
+            get;
+            set;
+        }
+
+        public DetallesContactoPage(Contacto contacto)
         {
             InitializeComponent();
+
+            Contacto = contacto;
+
+            nombreLabel.Text = $"{Contacto.Nombre} {Contacto.Apellido}";
+            telefonoLabel.Text = Contacto.Telefono;
+            correoLabel.Text = Contacto.Email;
         }
     }
 }
